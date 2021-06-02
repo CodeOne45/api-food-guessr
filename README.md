56f33edd94cd8178b841c137b75e033e58db9641
Aman KUMAR <57890543+CodeOne45@users.noreply.github.com>
2021-06-03 00:42:29 +0200

    feat: Added User DB + connection to MOngo DB

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # api-food-guessr
+
 Api to collecte food and manage login + register of a user
 
-# API Doc 
+# API Doc
+
 - get list of all meals : https://api-food-guessr.herokuapp.com/meals
 - get a random meal : https://api-food-guessr.herokuapp.com/meals/random
+
+# Source
+
+https://jasonwatmore.com/post/2018/06/14/nodejs-mongodb-simple-api-for-authentication-registration-and-user-management
+https://github.com/Automattic/mongoose/issues/6879
+https://bezkoder.com/node-js-mongodb-auth-jwt/
--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+require("rootpath")();
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
-app.use(cors());
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const jwt = require("_helpers/jwt");
+const errorHandler = require("_helpers/error-handler");
 
 const PORT = process.env.PORT || 8080;
 //const meals = require("./meals.json");
 var fs = require("fs");
-var meals = fs.readFileSync("meals.json", "utf8");
+var meals = fs.readFileSync("_helpers/meals.json", "utf8");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use("/users", require("./users/users.controller"));
+
+// global error handler
+app.use(errorHandler);
 
 app.get("/meals", (req, res) => {
   res.status(200).send(meals);
