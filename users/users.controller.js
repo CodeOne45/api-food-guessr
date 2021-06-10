cda9d1c79a38ecba786b9457e3d8abf66ea5ce63
Aman KUMAR <57890543+CodeOne45@users.noreply.github.com>
2021-06-10 17:01:00 +0200

    feat: Auth after regisster

--- users/users.controller.js
+++ users/users.controller.js
@@ -24,11 +24,18 @@ function authenticate(req, res, next) {
 }
 
 function register(req, res, next) {
-  userService
+  await userService
     .create(req.body)
     .then(() => res.json({}))
     .catch((err) => next(err));
-  authenticate;
+  userService
+    .authenticate(req.body)
+    .then((user) =>
+      user
+        ? res.json(user)
+        : res.status(400).json({ message: "Username or password is incorrect" })
+    )
+    .catch((err) => next(err));
 }
 
 function getAll(req, res, next) {
